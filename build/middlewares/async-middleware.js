58b30a07a196501cf1536e1cc3d91bea1fe8012a
Umair Farooqui <info.mufazmi@gmail.com>
2023-04-15 23:23:33 +0530

    Removed : Build

--- build/middlewares/async-middleware.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-//@ts-ignore
-module.exports = asyncMiddleware => (req, res, next) => Promise.resolve(asyncMiddleware(req, res, next)).catch(next);
--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,6 @@ app.use((req:Request,res:Response,next:NextFunction)=>{
     return next(ErrorHandler.notFound())
 })
 
-
 //Error Middleware
 app.use(errorMiddleware)
 
