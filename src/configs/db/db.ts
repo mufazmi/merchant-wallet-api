9d0c669459a32d6eae61aed4fc48e51ffb0d2354
Umair Farooqui <info.mufazmi@gmail.com>
2023-04-17 20:18:38 +0530

    Updated ConnectionFile

--- src/configs/db/db.ts
+++ src/configs/db/db.ts
@@ -1,19 +1,31 @@
 import { Model, Dialect, Sequelize } from 'sequelize';
 import config from "../config";
 
-console.log({config})
+console.log({ config })
 
-const db = new Sequelize(config.DB_NAME,config.DB_USER,config.DB_PASS,{
-    host:config.DB_HOST,
-    dialect:config.DB_DIALECT 
+const db = new Sequelize(config.DB_NAME, config.DB_USER, config.DB_PASS, {
+    host: config.DB_HOST,
+    dialect: config.DB_DIALECT
 });
 
-try{
-    db.authenticate();
-    console.log("SUCCESSFULLY CONNECTED WITH DATABASE SERVER")
+// try{
+//     db.authenticate();
+//     console.log("SUCCESSFULLY CONNECTED WITH DATABASE SERVER")
+// }
+// catch(e:any){
+//     console.log("FAILED TO CONNECT WITH DATABASE SERVER ====>",e)
+// }
+
+async function connectToDatabase() {
+    try {
+        await db.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 }
-catch(e:any){
-    console.log("FAILED TO CONNECT WITH DATABASE SERVER")
-}
+
+connectToDatabase();
+
 
 export default db;
