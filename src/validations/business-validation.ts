f559d5a062d906b7054a5919562badb91f37fe31
mufazmi <info.mufazmi@gmail.com>
2023-04-20 11:17:00 +0530

    implmented : validations

--- src/validations/business-validation.ts
+++ src/validations/business-validation.ts
@@ -4,15 +4,22 @@ class BusinessValidation {
 
     create = Joi.object({
         name:Joi.string().min(2).max(100).required(),
-        code : Joi.string().min(2).max(10).required(),
-        state_id : Joi.string().uuid().required(),
-        status: Joi.boolean().default(true)
+        legal_name:Joi.string().min(2).max(100).required(),
+        pan:Joi.string().min(10).max(10).required(),
+        gst:Joi.string().min(15).max(15).required(),
+        no:Joi.string().min(2).max(20).required(),
+        type:Joi.string().min(2).max(100).required(),
+        status: Joi.string().optional()
     });
 
     update = Joi.object({        
         name:Joi.string().min(2).max(100).optional(),
-        status: Joi.boolean().default(true),
-        state_id : Joi.string().uuid().optional(),
+        legal_name:Joi.string().min(2).max(100).optional(),
+        pan:Joi.string().min(10).max(10).optional(),
+        gst:Joi.string().min(15).max(15).optional(),
+        no:Joi.string().min(2).max(20).optional(),
+        type:Joi.string().min(2).max(100).optional(),
+        status: Joi.string().optional()
     });
 
 }
