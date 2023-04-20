4f4437d18a5228246fc7f16caa394baae9addf96
mufazmi <info.mufazmi@gmail.com>
2023-04-20 11:10:43 +0530

    Added Busines Service and controller

---
+++ src/controllers/business-controller.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from "express"
+import businessValidation from "../validations/business-validation"
+import responseSuccess from "../utils/response";
+import ErrorHandler from "../utils/error-handler";
+import Messages from '../utils/messages';
+import businessService from "../services/business-service";
+
+
+class BusinessController {
+
+    create = async (req: Request, res: Response, next: NextFunction) => {
+        const body = await businessValidation.create.validateAsync(req.body);
+        const data = await businessService.create(body);
+        return data ? responseSuccess({ res: res, message: Messages.CITY.CITY_CREATED }) : next(ErrorHandler.serverError(Messages.CITY.CITY_CREATION_FAILED));
+    }
+
+    findOne = async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.params;
+        const data = await businessService.findAll({ id });
+        return data ? responseSuccess({ res: res, message: Messages.CITY.CITY_FOUND, data: data }) : next(ErrorHandler.notFound(Messages.CITY.CITY_NOT_FOUND));
+
+    }
+
+    findAll = async (req: Request, res: Response, next: NextFunction) => {
+        const data = await businessService.findAll({});
+        return data.length > 1 ? responseSuccess({ res: res, message: Messages.CITY.CITY_FOUND, data: data }) : next(ErrorHandler.notFound(Messages.CITY.CITY_NOT_FOUND));
+    }
+
+    update = async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.params;
+        const body = await businessValidation.update.validateAsync(req.body);
+        const template = await businessService.findOne({ id });
+        if (!template)
+            return next(ErrorHandler.notFound(Messages.CITY.CITY_NOT_FOUND))
+
+        const data = await businessService.update({ id }, body);
+        return data ? responseSuccess({ res: res, message: Messages.CITY.CITY_UPDATED }) : next(ErrorHandler.serverError(Messages.CITY.CITY_UPDATE_FAILED));
+    }
+
+
+    destroy = async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.params;
+        const data = await businessService.destroy({id});
+        return data ? responseSuccess({ res: res, message: Messages.CITY.CITY_DELATED }) : next(ErrorHandler.notFound(Messages.CITY.CITY_DELETE_FAILED));
+    }
+}
+
+export default new BusinessController
