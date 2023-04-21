215109c8157115ce8348a011020426dc38a53ebc
Umair Farooqui <info.mufazmi@gmail.com>
2023-04-22 01:30:20 +0530

    Added File Upload Hanlder

--- src/controllers/kyc-document-controller.ts
+++ src/controllers/kyc-document-controller.ts
@@ -7,24 +7,9 @@ import kycDocumentService from "../services/kyc-document-service";
 import { AuthRequest } from "../interfaces/interface";
 import { InferAttributes } from "sequelize";
 import KycDocumentModel from "../models/kyc-document-model";
-import multer, { StorageEngine } from 'multer';
 
 
 class KycDocumentController {
-
-    storageEngine: StorageEngine = multer.diskStorage({
-        destination: (req: Request, file: File, cb: (error: Error | null, destination: string) => void) => {
-          if (['pan_front', 'aadhar_front', 'aadhar_back', 'proof'].includes(file.fieldname)) {
-            cb(null, './storage/images');
-          } else {
-            cb(new Error('Invalid field name'), '');
-          }
-        },
-        filename: (req: Request, file: File, cb: (error: Error | null, filename: string) => void) => {
-          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-          cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-        },
-      });
 
     create = async (req: AuthRequest, res: Response, next: NextFunction) => {
         const { id } = req.merchant
