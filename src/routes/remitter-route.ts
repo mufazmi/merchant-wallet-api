27e1977b6b532be4a1304a1639a45b759328a26e
mufazmi <info.mufazmi@gmail.com>
2023-05-02 12:16:32 +0530

    Added Search & Add Remitter's Route

---
+++ src/routes/remitter-route.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import remitterController from '../controllers/remitter-controller';
+const am = require('../middlewares/async-middleware');
+
+const router = express.Router();
+
+router.post('/',am(remitterController.create))
+router.get('/',am(remitterController.findAll))
+router.get('/:id',am(remitterController.findOne))
+router.patch('/:id',am(remitterController.update))
+router.delete('/:id',am(remitterController.destroy))
+
+export default router;
