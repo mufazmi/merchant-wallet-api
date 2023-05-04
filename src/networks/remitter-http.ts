875de3fe843606ad00260494a0d792843d2c3cba
mufazmi <info.mufazmi@gmail.com>
2023-05-04 10:34:12 +0530

    Implemented : Remitters & Recipient Http calls

--- src/networks/remitter-http.ts
+++ src/networks/remitter-http.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import crypto from 'crypto';
+import { connected } from 'process';
 import qs from 'qs';
 
 interface RemitterResponse {
@@ -23,7 +24,7 @@ class RemitterHttp {
   }
 
 
-  public async getRemitterResendOtp(mobile: string): Promise<RemitterResponse | null> {
+  public async resetOtp(mobile: string): Promise<RemitterResponse | null> {
     try {
       const url = `https://api.eko.in:25002/ekoicici/v2/customers/mobile_number:${mobile}/otp`;
       const data = qs.stringify({
@@ -32,11 +33,70 @@ class RemitterHttp {
         pipe: '9',
       });
       const response = await axios.post(url, data, { headers: this.headers });
-      return response.data;
+      return { data: response.data };
     } catch (error) {
-      return null;
+      return { error };
     }
   }
 
-  
+  public async getRemitters(mobile: string): Promise<RemitterResponse> {
+    try {
+      const url = `https://api.eko.in:25002/ekoicici/v2/customers/mobile_number:${mobile}`;
+      const data = qs.stringify({
+        initiator_id: '9433461804',
+        user_code: '31739001',
+      });
+      const response = await axios.get(url, { headers: this.headers, params: data });
+      if (response.data.response_type_id != '-1') {
+        return {
+          data: response.data,
+        };
+      } else {
+        return {
+          error: response.data,
+        };
+      }
+    } catch (error) {
+      return {
+        error,
+      };
+    }
+  }
+
+  verifyRemitter = async (otp: number, otp_ref_id: string, mobile: string) => {
+    try {
+      const remitterResponse = {
+        method: 'PUT',
+        url: `https://api.eko.in:25002/ekoicici/v2/customers/verification/otp:${otp}`,
+        headers: this.headers,
+        data: qs.stringify({
+          initiator_id: '9433461804',
+          id_type: 'mobile_number',
+          id: mobile,
+          otp_ref_id: otp_ref_id,
+          user_code: '31739001',
+          pipe: '9'
+        })
+      };
+
+      let response = await axios.request(remitterResponse)
+      if (response.data.response_type_id == '23')
+        return {
+          data: response.data
+        }
+      else
+        return {
+          error: response.data
+        }
+
+    } catch (error) {
+      console.log(error)
+      return {
+        data: error
+      }
+    }
+  }
+
 }
+
+export default RemitterHttp
