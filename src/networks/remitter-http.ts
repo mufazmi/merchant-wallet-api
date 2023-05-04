dd2c5fa2bb88ae80e7eb4211b036b9a17f77724d
mufazmi <info.mufazmi@gmail.com>
2023-05-04 10:04:25 +0530

    Implemented : implemented resend otp api

--- src/networks/remitter-http.ts
+++ src/networks/remitter-http.ts
@@ -3,55 +3,40 @@ import crypto from 'crypto';
 import qs from 'qs';
 
 interface RemitterResponse {
-  statusCode: number;
-  status: string;
   data?: any;
   error?: any;
 }
 
-const getRemitterResendOtp = async (mobile: string): Promise<RemitterResponse> => {
-  try {
+class RemitterHttp {
+  private readonly headers: AxiosRequestConfig['headers'];
+
+  constructor() {
     const secretKey = new Date().getTime().toString();
     const key = 'b977803d-0218-456e-a676-79de8c42f4b6';
     const hash = crypto.createHmac('sha256', secretKey).update(key).digest('base64');
-    const remitterResponse: AxiosRequestConfig = {
-      method: 'POST',
-      url: `https://api.eko.in:25002/ekoicici/v2/customers/mobile_number:${mobile}/otp`,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        accept: 'application/json',
-        developer_key: '0d13fefbdd3d507c3a1485e6694d4197',
-        // 'secret-key': hash,
-        // 'secret-key-timestamp': secretKey,
-      },
-      data: qs.stringify({
+    this.headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'accept': 'application/json',
+      'developer_key': '0d13fefbdd3d507c3a1485e6694d4197',
+      'hash': hash,
+    };
+  }
+
+
+  public async getRemitterResendOtp(mobile: string): Promise<RemitterResponse | null> {
+    try {
+      const url = `https://api.eko.in:25002/ekoicici/v2/customers/mobile_number:${mobile}/otp`;
+      const data = qs.stringify({
         initiator_id: '9433461804',
         user_code: '31739001',
         pipe: '9',
-      }),
-    };
-
-    const response = await axios.request(remitterResponse);
-    console.log(response);
-    if (response.data.response_type_id == '321') {
-      return {
-        statusCode: 200,
-        status: 'Success',
-        data: response.data,
-      };
-    } else {
-      return {
-        statusCode: 404,
-        status: 'Error',
-        error: response.data,
-      };
+      });
+      const response = await axios.post(url, data, { headers: this.headers });
+      return response.data;
+    } catch (error) {
+      return null;
     }
-  } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 404,
-      status: 'Error',
-      data: error,
-    };
   }
-};
+
+  
+}
